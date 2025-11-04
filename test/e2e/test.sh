b555e692c8a5f4097867e1b3b1a7ef5bb80929a7
eli <eli@permit.io>
2025-11-03 21:53:31 -0600

    Improve e2e test debugging with echo statements

--- test/e2e/test.sh
+++ test/e2e/test.sh
@@ -7,7 +7,12 @@ helm test -n opal --logs myopal
 
 DATA_URL="http://myopal-opal-client:8181/v1/data"
 
-[ "$(kubectl run -n opal curl-test --image=curlimages/curl:latest --rm -i --restart=Never -- curl -s ${DATA_URL}/users)" != "{}" ]
+# Check that users data is present initially
+RESULT=$(kubectl run -n opal curl-test --image=curlimages/curl:latest --rm -i --restart=Never -- curl -s ${DATA_URL}/users)
+echo "Initial users: $RESULT"
+[ "$RESULT" != "{}" ]
+
+# Run the update script
 if [ -z $MSYSTEM ]; then
   kubectl exec -n opal service/myopal-opal-server -- /opt/e2e/policy-repo-data/upd.sh
 else
@@ -15,5 +20,13 @@ else
 fi
 
 sleep 7
-[ "$(kubectl run -n opal curl-test --image=curlimages/curl:latest --rm -i --restart=Never -- curl -s ${DATA_URL}/users)" == "{}" ]
-[ "$(kubectl run -n opal curl-test --image=curlimages/curl:latest --rm -i --restart=Never -- curl -s ${DATA_URL}/losers)" != "{}" ]
+
+# Check that users data is empty after update
+RESULT=$(kubectl run -n opal curl-test --image=curlimages/curl:latest --rm -i --restart=Never -- curl -s ${DATA_URL}/users)
+echo "After update users: $RESULT"
+[ "$RESULT" == "{}" ]
+
+# Check that losers data is present
+RESULT=$(kubectl run -n opal curl-test --image=curlimages/curl:latest --rm -i --restart=Never -- curl -s ${DATA_URL}/losers)
+echo "Losers data: $RESULT"
+[ "$RESULT" != "{}" ]
